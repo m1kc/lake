6c8843c56d02c2cb349fe59fbd930694c75f657c
m1kc (Max Musatov) <m1kc@yandex.ru>
2016-11-18 21:17:52 +0300

    Improve the netspeed plugin
    - Allow specifying several network interfaces at once;
    - Don't fail when an interface becomes unavailable.

--- extra/m1kc/lake-800-netspeed.lua
+++ extra/m1kc/lake-800-netspeed.lua
@@ -1,17 +1,31 @@
 return function(lake)
-	local interface = "wlp2s0"  -- change this to fit your needs
+	local ifaces = {"enp0s20u2", "wlp2s0"}  -- change this to fit your needs
 	local wibox = lake.ask "wibox"
 	local widgets = lake.ask "vicious.widgets"
 	local netwidget = wibox.widget.textbox()
 	for s = 1, lake.screens() do
 		lake.add_to_right(netwidget, s)
 	end
+
+	local to_s = function(x)
+		if x == nil then
+			return "..."
+		end
+		return x
+	end
+
 	lake.every_second(function()
 		local data = widgets.net()
-		netwidget:set_text(
-			"↓"..data["{"..interface.." down_kb}"].."kbps"
-			.." "..
-			"↑"..data["{"..interface.." up_kb}"].."kbps"
-		)
+
+		local out = ""
+		for i,v in ipairs(ifaces) do
+			if i > 1 then out = out .. " " end
+			out = out ..
+				"↓"..to_s(data["{"..v.." down_kb}"]).."kbps"
+				.." "..
+				"↑"..to_s(data["{"..v.." up_kb}"]).."kbps"
+		end
+
+		netwidget:set_text(out)
 	end)
 end
