6b39dbf05b9d281c0081974fce65d1203550c8cd
m1kc (Max Musatov) <m1kc@yandex.ru>
2016-11-18 21:19:14 +0300

    Remove xscreensaver from autorun (it was buggy on my machine)

--- extra/lake-999-autorun.lua
+++ extra/lake-999-autorun.lua
@@ -52,6 +52,6 @@ return function(lake)
 
 	run_once(awful, lfs, "/usr/lib/gnome-settings-daemon/gnome-settings-daemon-localeexec")
 	run_once(awful, lfs, "compton", "compton -I 0.05 -O 0.05 -o 0.15")
-	run_once(awful, lfs, "xscreensaver", "xscreensaver -no-splash")
+	--run_once(awful, lfs, "xscreensaver", "xscreensaver -no-splash")
 	run_once(awful, lfs, "volumeicon")
 end
